f22b16af97aca84993d4ad8e0c05ebee87ef942d
kertalmew <n.zbitnev@g.nsu.ru>
2018-12-20 03:59:23 +0700

    TB-30 additional endpoints
    Signed-off-by: byDimasik <diman0497@mail.ru>

--- src/main/java/ru/nsu/fit/telegrambot/controller/JiraWebHookController.java
+++ src/main/java/ru/nsu/fit/telegrambot/controller/JiraWebHookController.java
@@ -3,13 +3,31 @@ package ru.nsu.fit.telegrambot.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.nsu.fit.telegrambot.dto.JiraCommnetEventDto;
+import ru.nsu.fit.telegrambot.dto.JiraFeatureEventDto;
+import ru.nsu.fit.telegrambot.dto.JiraIssueEventDto;
 import ru.nsu.fit.telegrambot.dto.JiraSprintEventDto;
 
 
 @RestController
 public class JiraWebHookController {
-    @PostMapping(path = "/hook")
-    public void webHook(@RequestBody JiraSprintEventDto event) {
+    @PostMapping(path = "/sprint")
+    public void springHook(@RequestBody JiraSprintEventDto event) {
+        System.out.println(event);
+    }
+
+    @PostMapping(path = "/commentary")
+    public void commentaryHook(@RequestBody JiraCommnetEventDto event) {
+        System.out.println(event);
+    }
+
+    @PostMapping(path = "/feature")
+    public void featureHook(@RequestBody JiraFeatureEventDto event) {
+        System.out.println(event);
+    }
+
+    @PostMapping(path = "/issue")
+    public void issueHook(@RequestBody JiraIssueEventDto event) {
         System.out.println(event);
     }
 }
