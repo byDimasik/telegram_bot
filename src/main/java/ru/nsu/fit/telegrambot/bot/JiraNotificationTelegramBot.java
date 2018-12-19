79a2e91f7f28020a2ac17f92c5956ce19a53a796
white <lavrinenko.wo@gmail.com>
2018-12-20 03:34:11 +0700

    TB-61 Added basic user registration

--- src/main/java/ru/nsu/fit/telegrambot/bot/JiraNotificationTelegramBot.java
+++ src/main/java/ru/nsu/fit/telegrambot/bot/JiraNotificationTelegramBot.java
@@ -7,6 +7,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.nsu.fit.telegrambot.config.TelegramBotConfig;
+import ru.nsu.fit.telegrambot.model.EventModel;
+import ru.nsu.fit.telegrambot.repository.EventRepository;
 
 import java.util.List;
 
@@ -15,10 +17,13 @@ public class JiraNotificationTelegramBot extends TelegramLongPollingBot {
 
     private static final String TG_PREFIX = "@";
     private final TelegramBotConfig telegramBotConfig;
+    private final EventRepository eventRepository;
 
     @Autowired
-    public JiraNotificationTelegramBot(TelegramBotConfig telegramBotConfig) {
+    public JiraNotificationTelegramBot(TelegramBotConfig telegramBotConfig,
+                                       EventRepository eventRepository) {
         this.telegramBotConfig = telegramBotConfig;
+        this.eventRepository = eventRepository;
     }
 
     @Override
@@ -27,17 +32,21 @@ public class JiraNotificationTelegramBot extends TelegramLongPollingBot {
                 .setChatId(update.getMessage().getChatId())
                 .setText(update.getMessage().getText());
 
+        if ("/start".equals(update.getMessage().getText())) {
+            EventModel eventModel = new EventModel(update.getMessage().getChatId());
+            eventRepository.save(eventModel);
+        }
+
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
     public void onUpdatesReceived(List<Update> updates) {
-        for (Update update: updates) {
+        for (Update update : updates) {
             onUpdateReceived(update);
         }
     }
