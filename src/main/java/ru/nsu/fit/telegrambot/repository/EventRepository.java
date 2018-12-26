d4d9498aa1e92b6e408b7754e77cd0e5919eb22e
white <lavrinenko.wo@gmail.com>
2018-12-27 00:14:26 +0700

    TB-38 Added repository methods to manage with issue event types

--- src/main/java/ru/nsu/fit/telegrambot/repository/EventRepository.java
+++ src/main/java/ru/nsu/fit/telegrambot/repository/EventRepository.java
@@ -4,7 +4,43 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.nsu.fit.telegrambot.model.EventModel;
 
+import java.util.List;
+
+/**
+ * Repository to manage with {@link EventModel} entity
+ */
 @Repository
 public interface EventRepository extends CrudRepository<EventModel, Long> {
 
+    /**
+     * Find all chat ids to send issue create event
+     *
+     * @param issueCreate issue create flag
+     * @return list of chat ids
+     */
+    List<Long> findAllChatIdByIssueCreateIs(Boolean issueCreate);
+
+    /**
+     * Find all chat ids to send issue update event
+     *
+     * @param issueUpdate issue update flag
+     * @return list of chat ids
+     */
+    List<Long> findAllChatIdByIssueUpdateIs(Boolean issueUpdate);
+
+    /**
+     * Find all chat ids to send issue delete event
+     *
+     * @param issueDelete issue delete flag
+     * @return list of chat ids
+     */
+    List<Long> findAllChatIdByIssueDeleteIs(Boolean issueDelete);
+
+    /**
+     * Find all chat ids to send issue work log event
+     *
+     * @param issueWorkLog issue work log flag
+     * @return list of chat ids
+     */
+    List<Long> findAllChatIdByIssueWorkLogIs(Boolean issueWorkLog);
 }
