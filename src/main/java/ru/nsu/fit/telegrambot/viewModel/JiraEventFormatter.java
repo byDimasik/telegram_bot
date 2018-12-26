bb7afa292d4fd8cb34acdf543f82355b9fb6ddda
byDimasik <diman0497@mail.ru>
2018-12-27 00:33:31 +0700

    TB-73 issue formatter fix
    Signed-off-by: byDimasik <diman0497@mail.ru>

--- src/main/java/ru/nsu/fit/telegrambot/viewModel/JiraEventFormatter.java
+++ src/main/java/ru/nsu/fit/telegrambot/viewModel/JiraEventFormatter.java
@@ -6,7 +6,7 @@ import ru.nsu.fit.telegrambot.dto.JiraIssueEventDto;
 public class JiraEventFormatter {
     public String parseIssueEvent(JiraIssueEventDto event) {
         String message = event.getIssue().getFields().getCreator().getDisplayName();
-        message += parseEventType(event);
+        message += " " + parseEventType(event);
         message += " " + event.getIssue().getFields().getIssuetype().getName().toLowerCase();
         message += " " + event.getIssue().getKey();
         message += " \"" + event.getIssue().getFields().getSummary() + "\"";
@@ -17,11 +17,11 @@ public class JiraEventFormatter {
         String eventType = event.getWebhookEvent();
         String resultMessage = "";
         if (eventType.contains("created")) {
-            resultMessage = " create";
+            resultMessage = "create";
         } else if (eventType.contains("updated")) {
-            resultMessage = " update";
+            resultMessage = "update";
         } else if (eventType.contains("deleted")){
-            resultMessage = " delete";
+            resultMessage = "delete";
         }
         return resultMessage;
     }
